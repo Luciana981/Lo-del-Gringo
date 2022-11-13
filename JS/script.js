c4e4377a44f6e896c34d4c9cfe6108bfcce42c11
Luciana  Agüero <luciana.aguero.epet20@gmail.com>
2022-11-13 04:22:00 -0300

    Se borro la funcion en js

--- JS/script.js
+++ JS/script.js
@@ -11,16 +11,5 @@ let charIndex = 0;
 //#endregion
 
 //#region 
-function type() {
-    if(charIndex < textArray[textArrayIndex].length){//aqui se inserta el array de los textos
-        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(type, typingDelay);
-    }else{
-        //aqui se borra el texto para proceder a otro
-    }
-}
-
-type();
 
 //#endregion
