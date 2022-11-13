ca278bbfe3e03b28399856423b2fe39169dc23af
Luciana  Agüero <luciana.aguero.epet20@gmail.com>
2022-11-13 05:37:30 -0300

    Se termino la funcion de type y se agrego un addEventListener

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,8 @@ const typedTextSpan = document.querySelector(".texto-tipeado");
 const cursorSpan = document.querySelector (".cursor");
 
 const textArray = ["Familiar", "Rico", "Amabilidad", "Pet Friendly"];//definiendo los textos de texto-tipeado
-const typingDelay = 200;
+//se definen los retrasos en milisegundos
+const typingDelay = 200; 
 const erasingDelay = 100;
 const newTextDelay = 2000;
 let textArrayIndex = 0;
@@ -13,14 +14,19 @@ let charIndex = 0;
 //#region 
 function type() {
     if(charIndex < textArray[textArrayIndex].length){//aqui se inserta el array de los textos
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
         typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-        charIndex++;
+        charIndex++;//se agrega uno para proceder al caracter siguiente
         setTimeout(type, typingDelay);
     }else{
-        //aqui se borra el texto para proceder a otro
+     cursorSpan.classList.remove("typing");
+     setTimeout(erase, newTextDelay); //se agrega un retraso de 2000 milisegundos antes de pasar a la funcion de erase
     }
 }
 
-type();
+//agregado para tener control del llamado de la funcion
+document.addEventListener("DOMContentLoaded", function() {//cuando todos los elementos DOM carguen, se va a ejecutar la funcion
+    if (textArray.length) setTimeout(type, newTextDelay + 250);//se le agrega un retraso de 250 milisegundos antes de ejecutar la funcion 
+});
 
 //#endregion
