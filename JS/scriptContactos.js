f609ccde8d8cf287fbc29da6c128081f5def657f
Luciana  Agüero <luciana.aguero.epet20@gmail.com>
2022-11-14 14:11:15 -0300

    Se crearon las variables por id

---
+++ JS/scriptContactos.js
@@ -0,0 +1,4 @@
+let enviar = document.getElementById('enviar'); //se crean las variables
+let mail = document.getElementById('mail');
+let usuario = document.getElementById('usuario');
+let password = document.getElementById('password');
