27ab85e9fdf26ea9aff4ae0b1492534f67ddddbe
Rosario González <rosario.gonzalez.epet20@gmail.com>
2022-11-13 22:37:09 -0300

    Se agrego un document

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,3 @@
 let preveiwContainer = document.querySelector('.products-preview');
 let previewBox = preveiwContainer.querySelectorAll('.preview');
+document.querySelectorAll('.products-container .product').forEach(product =>{});
