8f98f6bf4468dba8133eddf3719d3622030c9b88
Rosario González <rosario.gonzalez.epet20@gmail.com>
2022-11-13 22:41:25 -0300

    Se agregan funciones al proveiwcontainer

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,11 @@
 let preveiwContainer = document.querySelector('.products-preview');
 let previewBox = preveiwContainer.querySelectorAll('.preview');
 document.querySelectorAll('.products-container .product').forEach(product =>{
-    product.onclick = () =>{};
+    product.onclick = () =>{
+      preveiwContainer.style.display = 'flex';
+      let name = product.getAttribute('data-name');
+      previewBox.forEach(preview =>{
+       
+      });
+    };
   });
