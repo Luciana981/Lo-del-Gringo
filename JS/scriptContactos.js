99b40cfb86eb7da1397eadc719732b9a3b232a03
Luciana  Agüero <luciana.aguero.epet20@gmail.com>
2022-11-14 14:30:21 -0300

    Se termino la funcion de mover

--- JS/scriptContactos.js
+++ JS/scriptContactos.js
@@ -2,9 +2,23 @@ let enviar = document.getElementById('enviar'); //se crean la variable del boton
 let mail = document.getElementById('mail');//se creo la variable del campo de correo
 let usuario = document.getElementById('usuario');//se creo la variable del campo de usuario
 let password = document.getElementById('password');//se creo la variable del campo de la contra
-var sensorM = 0;
-var sensorC = 0;
+var sensor1 = 0;
 
 function mover() {//se crea la funcion para que el boton de envio no se presione hasta que los campos esten llenos
-    
+    if (usuario.value = null){//verifica que el campo del usuario este lleno
+        sensor1 = sensor1 + 1;
+    }
+
+    if(mail.value = null){
+        sensor1 = sensor1 + 1;
+    }
+
+    if(password.value = null){
+        sensor1 = sensor1 + 1;
+    }
+
+    if(senson1 >= 1){
+        enviar.classList.toggle('hover');
+    }
+    sensor1 = 0;
 }
