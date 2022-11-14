3f4ae76649909320629c3519c34d336412fcfc6f
Rosario González <rosario.gonzalez.epet20@gmail.com>
2022-11-14 16:43:25 -0300

    Se creo una funcion

--- JS/scriptContactos.js
+++ JS/scriptContactos.js
@@ -21,3 +21,4 @@ function mover() {//se crea la funcion para que el boton de envio no se presione
         sensorM = false;
 }
 
+function crearEnviar() {} //al hacer click que se cree un h1 y que se muevva todo el registro
