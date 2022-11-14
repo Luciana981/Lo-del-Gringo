43278bd0be7f64b4d6636df48c38381e10c39aef
Rosario González <rosario.gonzalez.epet20@gmail.com>
2022-11-14 16:09:31 -0300

    Se agregaron comentarios en js

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
-let preveiwContainer = document.querySelector('.products-preview');
-let previewBox = preveiwContainer.querySelectorAll('.preview');
-document.querySelectorAll('.products-container .product').forEach(product =>{
-    product.onclick = () =>{
+let preveiwContainer = document.querySelector('.products-preview'); /*querySelector() Devuelve el primer elemento del documento*/
+let previewBox = preveiwContainer.querySelectorAll('.preview'); /* querySelectorAll() de un Element devuelve una NodeList estática, que representa una lista de elementos del documento que coinciden con el grupo de selectores indicados. */
+document.querySelectorAll('.products-container .product').forEach(product =>{ /*forEach() ejecuta la función callback una vez por cada elemento del array*/
+    product.onclick = () =>{ /*onclick ejecuta una cierta funcionalidad cuando se hace clic en un botón.*/
       preveiwContainer.style.display = 'flex';
-      let name = product.getAttribute('data-name');
+      let name = product.getAttribute('data-name'); /*getAttribute() devuelve el valor del atributo especificado en el elemento.*/
       previewBox.forEach(preview =>{
         let target = preview.getAttribute('data-target');
         if(name == target){
