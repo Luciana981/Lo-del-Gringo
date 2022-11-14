4ce570ebbfc7dd43a1ebd6c2f31ab16630fa2850
Luciana  Agüero <luciana.aguero.epet20@gmail.com>
2022-11-14 14:16:40 -0300

    Se inicializaron los sensores M y C

--- JS/scriptContactos.js
+++ JS/scriptContactos.js
@@ -1,4 +1,10 @@
-let enviar = document.getElementById('enviar'); //se crean las variables
-let mail = document.getElementById('mail');
-let usuario = document.getElementById('usuario');
-let password = document.getElementById('password');
+let enviar = document.getElementById('enviar'); //se crean la variable del boton de envio
+let mail = document.getElementById('mail');//se creo la variable del campo de correo
+let usuario = document.getElementById('usuario');//se creo la variable del campo de usuario
+let password = document.getElementById('password');//se creo la variable del campo de la contra
+var sensorM = 0;
+var sensorC = 0;
+
+function mover() {//se crea la funcion para que el boton de envio no se presione hasta que los campos esten llenos
+
+}
