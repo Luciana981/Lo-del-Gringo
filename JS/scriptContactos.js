226decede057bdaaa9bf538a8dc168ea87f5e828
Luciana  Agüero <luciana.aguero.epet20@gmail.com>
2022-11-14 15:19:11 -0300

    Se esta intentando buscar el problema en js

--- JS/scriptContactos.js
+++ JS/scriptContactos.js
@@ -2,23 +2,25 @@ let enviar = document.getElementById('enviar'); //se crean la variable del boton
 let mail = document.getElementById('mail');//se creo la variable del campo de correo
 let usuario = document.getElementById('usuario');//se creo la variable del campo de usuario
 let password = document.getElementById('password');//se creo la variable del campo de la contra
-var sensor1 = 0;
+var sensorM = 0;
+var sensorC = 0;
 
 function mover() {//se crea la funcion para que el boton de envio no se presione hasta que los campos esten llenos
-        if (usuario.value = null || usuario.value == ''){//verifica que el campo del usuario este lleno
-        sensor1 = sensor1 + 1;
+
+    if (usuario.value === null || usuario.value === ''){//verifica que el campo del usuario este lleno
+        sensorM = sensorM + 1;
     }
 
-    if(mail.value = null || mail.value == ''){
-        sensor1 = sensor1 + 1;
+    if(mail.value === null || mail.value === ''){
+        sensorM = sensorM + 1;
     }
 
-    if(password.value = null || password.value == ''){
-        sensor1 = sensor1 + 1;
+    if(password.value === null || password.value === ''){
+        sensorM = sensorM + 1;
     }
 
-    if(senson1 >= 1){
+    if(sensorM >= 1){
         enviar.classList.toggle('hover');
     }
-    sensor1 = 0;
+    sensorM = 0;
 }
