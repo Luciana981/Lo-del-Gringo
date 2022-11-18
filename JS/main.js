3ad6b85fd86cd97eee0ed324c51aeb26b9484bdf
Rosario González <rosario.gonzalez.epet20@gmail.com>
2022-11-18 15:30:46 -0300

    Se agrego la funcion para cerrar el modal

--- JS/main.js
+++ JS/main.js
@@ -6,3 +6,8 @@ openModal.addEventListener('click', (e)=>{
     e.preventDefault ();
     modal.classList.add('modal-show');
 });
+
+cerrarModal.addEventListener('click', (e)=>{
+    e.preventDefault ();
+    modal.classList.remove('modal-show');
+});
