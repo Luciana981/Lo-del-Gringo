a7e4fafdecc353e680d05e07411134df37ac6458
Rosario González <rosario.gonzalez.epet20@gmail.com>
2022-11-18 15:10:41 -0300

    Modal agregaa una classlist}

--- JS/main.js
+++ JS/main.js
@@ -2,4 +2,5 @@ const boton = document.querySelector ('.compra');
 const modal = document.querySelector ('.modal')
 
 boton.addEventListener('click', () =>{
+    modal.classList.add('modal-show')
 });
